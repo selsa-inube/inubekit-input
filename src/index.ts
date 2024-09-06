3c39401dca8c7f82909b7cf4255f959919a36a81
cmarin001 <cmarin6051@gmail.com>
2024-09-06 18:24:01 -0500

    fix: added export of the components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { Input } from "./Inputs/Input";
+export { Textfield } from "./Inputs/Textfield";
+export { Emailfield } from "./Inputs/Emailfield";
+export { Phonefield } from "./Inputs/Phonefield";
+export { Numberfield } from "./Inputs/Numberfield";
+export { Moneyfield } from "./Inputs/Moneyfield";
+export { Searchfield } from "./Inputs/Searchfield";
 export type { IInput } from "./Inputs/Input";
